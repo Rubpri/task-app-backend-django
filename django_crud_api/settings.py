fed348b8ea8168feeef7aceac4a0bb25c486dbb7
Rubpri <rubenprietoserrano@gmail.com>
2024-04-12 12:28:36 +0200

    allowed hosts

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
-#                           "localhost,127.0.0.1,[::1]").split(",")
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
 
 # Application definition
